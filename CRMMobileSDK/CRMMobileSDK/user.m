af1d509d664c6ba21cddfb18c24a16e71becf317
Eli Shalom <71207769+elishapiiro@users.noreply.github.com>
2023-04-17 05:46:01 +0300

    Create user.m

---
+++ CRMMobileSDK/CRMMobileSDK/user.m
@@ -0,0 +1,16 @@
+@interface User : NSObject
+
+@property (nonatomic, strong) NSString *firstName;
+@property (nonatomic, strong) NSString *lastName;
+@property (nonatomic, strong) NSString *email;
+@property (nonatomic, strong) NSString *phone;
+@property (nonatomic, strong) NSString *address;
+@property (nonatomic, strong) NSString *city;
+@property (nonatomic, strong) NSString *state;
+@property (nonatomic, strong) NSString *postalCode;
+@property (nonatomic, strong) NSString *country;
+@property (nonatomic, strong) NSString *dateOfBirth;
+@property (nonatomic, strong) NSString *gender;
+@property (nonatomic, strong) NSString *occupation;
+
+@end
